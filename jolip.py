0f43fc7a39031451e13dbfd43e49165024029ac9
fernandogaytan1004-commits <fernando.gaytan1004@alumnos.udg.mx>
2026-02-03 15:55:01 -0600

    blocks

--- jolip.py
+++ jolip.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 jolipApp = Flask(__name__)
 
 @jolipApp.route ('/')
 def home():
-    return '<h1>pimis</h1>'
+    return render_template
 
 if __name__=='__main__':
     jolipApp.run(port=5000, debug=True)
