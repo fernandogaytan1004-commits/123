5431540030a14cccd94392cad4ee154a577f18ec
fernandogaytan1004-commits <fernando.gaytan1004@alumnos.udg.mx>
2026-02-11 17:47:37 -0600

    33rpm beta 3

--- 33rpm.py
+++ 33rpm.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import hashlib
+from flask import Flask, render_template, request, redirect, url_for
 import json
 
 app = Flask(__name__)
+
+app.secret_key = "super_secret_key"
+
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "mysql"   
+app.config["MYSQL_DB"] = "rpm33"
+ 
+
+mysql = MySQL(app)
 
 @app.route("/")
 def index():
@@ -21,5 +35,10 @@ def reproductor():
 def ventas():
     return render_template("ventas.html")
 
+@app.route("/auth")
+def auth():
+    return render_template("auth.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
+
